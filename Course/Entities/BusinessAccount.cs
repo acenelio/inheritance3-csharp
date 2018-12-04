6e1f997e9b9796b3f514a1ef09035454b1613cbf
Nelio Alves <acenelio@gmail.com>
2018-12-04 09:51:04 -0200

    Initial implementation

---
+++ Course/Entities/BusinessAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace Course.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) 
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount - 10.0;
+            }
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.0; // error
+        }
+    }
+}
